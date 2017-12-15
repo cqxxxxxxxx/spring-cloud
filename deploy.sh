194bded7ae643d2b43b3a0d4d0958be26c64dd13
BG307435 <jxchen35@best-inc.com>
2017-12-15 19:56:37 +0800

    xiugai

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+
 read -p '输入module   ' module
 read -p '输入profile   ' profile
 read -p '端口映射(8080:9000 )  ' port_map
+tempDir="$PWD"
+cd "/root/code/spring-cloud/$module"
 echo "$PWD"
 git pull
-docker run -v ~/.m2:/root/.m2 -v "$PWD":/tmp/build --rm -w /tmp/build maven:3.3.3-jdk-8 mvn clean package  -s docker/settings.xml -Dmaven.test.skip=true
+docker run -v ~/.m2:/root/.m2 -v "$tempDir":/tmp/build --rm -w /tmp/build maven:3.3.3-jdk-8 mvn clean package  -s docker/settings.xml -Dmaven.test.skip=true
 docker build --build-arg MODULE="$module" --build-arg PROFILE="$profile" -t $module .
 docker run -d -p $port_map -v ~/logs:/logs --name $module -e TZ=Asia/Shanghai $module
