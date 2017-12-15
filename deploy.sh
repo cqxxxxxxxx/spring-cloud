f0556d7a915448c926cffb72c380dc313c224e6c
BG307435 <jxchen35@best-inc.com>
2017-12-15 19:35:06 +0800

    脚本错误修改

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ read -p '端口映射(8080:9000 )  ' port_map
 echo "$PWD"
 git pull
 docker run -v ~/.m2:/root/.m2 -v "$PWD":/tmp/build --rm -w /tmp/build maven:3.3.3-jdk-8 mvn clean package  -s docker/settings.xml -Dmaven.test.skip=true
-docker build --build-arg APP_MODULE="$module" --build-arg APP_ENV="$profile" -t $module .
+docker build --build-arg MODULE="$module" --build-arg PROFILE="$profile" -t $module .
 docker run -d -p $port_map -v ~/logs:/logs --name $module -e TZ=Asia/Shanghai $module
