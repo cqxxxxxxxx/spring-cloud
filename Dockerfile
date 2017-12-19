ba21764ef243c99294fb8b52fab0e3a6722bc87a
BG307435 <jxchen35@best-inc.com>
2017-12-19 18:12:14 +0800

    脚本修改。。。

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,8 @@ MAINTAINER cqxxxxxxxx@gmail.com
 
 ARG MODULE
 ARG PROFILE=default
-ENV MODULE = ${MODULE}
-ENV PROFILE = ${PROFILE}
-ADD ${MODULE}/target/*.jar ${MODULE}.jar
-ENV JAVA_OPTS = '-Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=${PROFILE} '
-ENTRYPOINT [ "sh", "-c", "java ${JAVA_OPTS} -jar /${MODULE}.jar" ]
-
+ENV MODULE=${MODULE}
+ENV PROFILE=${PROFILE}
+ADD ${MODULE}/target/*.jar app.jar
+ENV JAVA_OPTS='-Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=${PROFILE}'
+ENTRYPOINT exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-read -p '输入module   ' module
-read -p '输入profile   ' profile
-read -p '端口映射(8080:9000 )  ' port_map
+read -p '输入module(eureka)   ' module
+read -p '输入profile(default)   ' profile
+read -p '端口映射(8080:8080)  ' port_map
 tempDir="$PWD"
-cd "/root/code/spring-cloud/$module"
+if [ -z $module ]; then
+    module='eureka'
+fi
+if [ -z $profile ]; then
+    profile='default'
+fi
+if [ -z $port_map ]; then
+    port_map='8080:8080'
+fi
+cd "$PWD/$module"
 echo "$PWD"
 git pull
-docker run -v ~/.m2:/root/.m2 -v "$tempDir":/tmp/build --rm -w /tmp/build maven:3.3.3-jdk-8 mvn clean package  -s docker/settings.xml -Dmaven.test.skip=true
+docker run -v ~/.m2:/root/.m2 -v "$PWD":/tmp/build --rm -w /tmp/build maven:3.3.3-jdk-8 mvn clean package -Dmaven.test.skip=true
+cd "$tempDir"
+echo "$module,$profile,$prot_map"
 docker build --build-arg MODULE="$module" --build-arg PROFILE="$profile" -t $module .
 docker run -d -p $port_map -v ~/logs:/logs --name $module -e TZ=Asia/Shanghai $module
