8677927b7ed75b8dfb1ea50df851286dee9a4921
BG307435 <jxchen35@best-inc.com>
2018-03-28 12:41:57 +0800

    VIRTUAL_HOST 用于nginx-proxy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 read -p '输入module(eureka)   ' module
 read -p '输入profile(default)   ' profile
 read -p 'host端口映射到80  ' port
+read -p '输入域名' domainName
 tempDir="$PWD"
 if [ -z $module ]; then
     module='eureka'
@@ -13,6 +14,9 @@ fi
 if [ -z $port ]; then
     port='80'
 fi
+if [ -z $domainName ]; then
+    domainName='cqxxxxxxxx.com'
+fi
 cd "$PWD/$module"
 echo "$PWD"
 git pull
@@ -20,4 +24,4 @@ docker run -v ~/.m2:/root/.m2 -v "$PWD":/tmp/build --rm -w /tmp/build maven:3.3.
 cd "$tempDir"
 echo "$module,$profile,$prot"
 docker build --build-arg MODULE="$module" --build-arg PROFILE="$profile" -t $module .
-docker run -d -p $port:80 -v ~/logs:/logs --name $module -e TZ=Asia/Shanghai $module
+docker run -d -p $port:80 -v ~/logs:/logs --name $module -e TZ=Asia/Shanghai -e VIRTUAL_HOST=$domainName $module
