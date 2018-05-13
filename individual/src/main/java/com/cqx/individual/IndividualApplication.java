a76ae7d38abace3294ea58731cb47f7552f4be36
cqxxxxxxxx <cqxxxxxxxx@gmail.com>
2018-05-13 21:39:47 +0800

    individual更新

--- individual/src/main/java/com/cqx/individual/IndividualApplication.java
+++ individual/src/main/java/com/cqx/individual/IndividualApplication.java
@@ -3,28 +3,34 @@ package com.cqx.individual;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * @author BG307435
  */
 @SpringBootApplication
-@RestController
+@Controller
 public class IndividualApplication {
 
     private static final Logger log = LoggerFactory.getLogger(IndividualApplication.class);
 
     @Autowired
     StringRedisTemplate stringRedisTemplate;
+    @Value("${hostname}")
+    String id;
 
     public static void main(String[] args) {
         SpringApplication.run(IndividualApplication.class, args);
     }
 
     @PostMapping("/redis")
+    @ResponseBody
     public String addKV(@RequestParam String key, @RequestParam String value) {
         log.info("addKV -- key:{},value:{}", key, value);
         stringRedisTemplate.opsForValue().set(key, value);
@@ -32,8 +38,15 @@ public class IndividualApplication {
     }
 
     @GetMapping("/redis/{key}")
+    @ResponseBody
     public String getV(@PathVariable String key) {
         log.info("getV -- key:{}", key);
         return stringRedisTemplate.opsForValue().get(key);
+    }
+
+    @GetMapping("/index")
+    public String toIndex(Model model) {
+        model.addAttribute("id", id);
+        return "index";
     }
 }
